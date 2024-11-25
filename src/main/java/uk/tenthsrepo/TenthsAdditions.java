39f082fc8f3ca41e4b388b2493160ac0d43488c4
Oliver Johns <mrojohns@proton.me>
2024-11-25 17:22:21 +0000

    Uploaded Initial Files and Template

---
+++ src/main/java/uk/tenthsrepo/TenthsAdditions.java
@@ -0,0 +1,16 @@
+package uk.tenthsrepo;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TenthsAdditions implements ModInitializer {
+	public static final String MOD_ID = "tenthsadditions";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
