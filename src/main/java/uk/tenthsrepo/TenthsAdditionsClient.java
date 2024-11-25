d53cb39ef069a0920727269ddfc518db2fbb39a4
Oliver Johns <mrojohns@proton.me>
2024-11-25 18:37:38 +0000

    Tutorial One finished

---
+++ src/main/java/uk/tenthsrepo/TenthsAdditionsClient.java
@@ -0,0 +1,9 @@
+package uk.tenthsrepo;
+import net.fabricmc.api.ClientModInitializer;
+
+public class TenthsAdditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
