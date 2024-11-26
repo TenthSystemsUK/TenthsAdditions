c9b4793ca6e4cd0a88dd9fad7a9711154460523f
Oliver Johns <mrojohns@proton.me>
2024-11-26 22:26:40 +0000

    Ore Gen pt.1

--- src/main/java/uk/tenthsrepo/TenthsAdditionsDataGenerator.java
+++ src/main/java/uk/tenthsrepo/TenthsAdditionsDataGenerator.java
@@ -2,10 +2,23 @@ package uk.tenthsrepo;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
+import uk.tenthsrepo.datagen.ModWorldGenerator;
+import uk.tenthsrepo.world.ModConfiguredFeatures;
 
 public class TenthsAdditionsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModWorldGenerator::new);
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+		registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ModConfiguredFeatures::boostrap);
+		// registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, ModConfiguredFeatures::boostrap);
 
 	}
 }
